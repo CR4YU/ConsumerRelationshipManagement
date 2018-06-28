cef213d460c2377c3eb7dfe1117b62e86861d15f
CR4YU <craayu@gmail.com>
2018-06-29 01:56:09 +0200

    Added logging and logging aspect

---
+++ src/main/java/org/example/crm/aspect/LoggingAspect.java
@@ -0,0 +1,38 @@
+package org.example.crm.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+	private static final Logger logger = LogManager.getLogger(LoggingAspect.class);
+
+
+	@Pointcut("execution(* org.example.crm.controller.*.*(..))")
+	private void controllerPackage() {}
+
+	@Pointcut("execution(* org.example.crm.service.*.*(..))")
+	private void servicePackage() {}
+
+	@Pointcut("execution(* org.example.crm.dao.*.*(..))")
+	private void daoPackage() {}
+
+	@Pointcut("controllerPackage() || servicePackage() || daoPackage()")
+	private void appFlow() {}
+
+
+	@Before("appFlow()")
+	public void before(JoinPoint joinPoint) {
+		String method = joinPoint.getSignature().toShortString();
+
+		logger.info("@Before advice on method: " + method);
+	}
+}
